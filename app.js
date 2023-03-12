0e391dc89ef5ba30d9f39c8981b7c6b7b1e8d2e9
AdoniTzadeq <neenakmathewk@gmail.com>
2023-03-12 17:08:40 +0530

    first change

---
+++ app.js
@@ -0,0 +1,141 @@
+// Task1: initiate app and run server at 3000
+const express = require('express');
+const Mongoose = require('mongoose');
+const cors =require('cors');
+const logger = require('morgan');
+const app =  express();
+
+
+
+
+const path=require('path');
+app.use(logger('dev'));
+app.use(cors())
+app.use(express.static(path.join(__dirname+'/dist/FrontEnd')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const sch= 
+    {
+        
+        name:String,
+        location:String,
+        position :String,
+        salary:Number
+    }
+const employeeModel= Mongoose.model(
+    "Employees",sch
+    
+)
+
+Mongoose.connect("mongodb+srv://ghuk:learningtolive@cluster0.uangpgd.mongodb.net/EmployeeDB?retryWrites=true&w=majority",{useNewUrlParser:true})
+
+
+
+//Task 2 : write api with error handling and appropriate api mentioned in the TODO below
+
+
+
+
+
+
+
+//TODO: get data from db  using api '/api/employeelist'
+
+
+app.get('/api/employeelist',async(req,res)=>{
+    try{
+    let data= await employeeModel.find()
+    res.json(data)
+    }
+    catch(error){
+        console.log('Error:',error)
+        res.status(400).json()
+    }
+})
+
+
+
+
+
+//TODO: get single data from db  using api '/api/employeelist/:id'
+
+
+app.get('/api/employeelist/:id',async(req,res)=>{
+    try{
+    let data= await employeeModel.findById(req.params.id)
+    res.json(data)
+    }
+
+    catch(error){
+        console.log('Err:',error)
+        res.status(400).json()
+    }
+})
+
+
+
+
+//TODO: send data from db using api '/api/employeelist'
+//Request body format:{name:'',location:'',position:'',salary:''}
+
+app.post('/api/employeelist',async(req,res)=>{
+    try{
+    let data= new employeeModel(req.body)
+    data.save()
+    res.json({status: "Success"})
+    }
+    catch(error){
+        console.log('Err:',error)
+        res.status(400).json()
+    }
+})
+
+
+
+
+
+//TODO: delete a employee data from db by using api '/api/employeelist/:id'
+
+
+
+app.delete('/api/employeelist/:id',async(req,res)=>{
+    try{
+    let data= await employeeModel.deleteOne(req.body)
+    res.json(data)
+    }
+
+    catch(error){
+        console.log('Err:',error)
+        res.status(400).json()
+    }
+})
+
+
+
+//TODO: Update  a employee data from db by using api '/api/employeelist'
+//Request body format:{name:'',location:'',position:'',salary:''}
+app.put('/api/employeelist',async(req,res)=>{
+    try{
+   
+   let data= await employeeModel.findOneAndUpdate({"_id": req.body._id}, req.body)
+   res.json(data)
+    }
+    catch(error){
+        console.log('Err:',error)
+        res.status(400).json()
+    }
+})
+
+
+app.listen(3000, ()=>{
+console.log('Server started')
+})
+
+//! dont delete this code. it connects the front end file.
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/Frontend/index.html'));
+});
+
+
+
